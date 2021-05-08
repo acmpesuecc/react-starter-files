927b6b610ac22e69d5cb18148b299169d28ce9ed
avinash-vk <avinash2000vk@gmail.com>
2021-05-08 23:42:29 +0530

    added file creation

--- src/commands/init/index.js
+++ src/commands/init/index.js
@@ -1,16 +1,57 @@
+import fs from 'fs';
 import clear from 'clear';
 import inquirer from 'inquirer';
 
 import { Intro } from '../../utils/interactiveOutputs';
 
+const createPagesFolder = () => {
+    fs.mkdirSync(process.cwd()+"/src/pages",{ recursive:true}, (error)=>{
+        if(error){
+            console.error("ERROR OCCURED:",error);
+            return false;
+        }
+        else {
+            return true;
+        }
+    })
+}
+
+const componentTemplate = (component) => {
+    return (
+    `import React from 'react';\n\nconst ${component} = () => {\n\treturn (\n\t\t<div>${component}</div>\n\t)\n};\n\nexport default ${component};`
+    )
+}
+
+const createPageFile = ({component}) => {
+    fs.writeFile(process.cwd()+`/src/pages/${component}.js`,componentTemplate(component),(error)=>{
+        if(error){
+            console.log("ERROR OCCURED:",error);
+            return false;
+        }
+        else {
+            return true;
+        }
+    })
+}
 const init = async (showIntro=true) => {
     clear();
     if (showIntro) Intro();
 
-    const pages = [];
+    const pages = [
+        {
+            name: "home",
+            component:"Home",
+            route:"/home"
+        },
+        {
+            name: "info",
+            component:"Info",
+            route:"/info"
+        }
+    ];
     
     console.log("\n Let's start by creating your pages. Type *qa to end the process right there:\n")
-    let flag=1;
+    let flag=1;/*
     while(flag){
         console.log("\n")
         let answers = await inquirer.prompt([
@@ -43,11 +84,15 @@ const init = async (showIntro=true) => {
         catch(err){
             console.log("ERROR", err)
         }
-    }
+    }*/
 
     console.info("\n✅ Done");
-    console.log("\nPages:",pages)
+    console.log("\nPages:",pages);
 
+    //flag= createPagesFolder(pages);
+    if(true) {
+        pages.forEach(page => createPageFile(page));
+    }
 }
 
 export default init;
