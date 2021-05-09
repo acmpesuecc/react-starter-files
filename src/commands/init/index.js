7bb07e562ba5b098776f283a6c9e1212ae7b0105
avinash-vk <avinash2000vk@gmail.com>
2021-05-09 22:02:07 +0530

    integrated everything

--- src/commands/init/index.js
+++ src/commands/init/index.js
@@ -1,62 +1,80 @@
-import fs from 'fs';
-import clear from 'clear';
-import inquirer from 'inquirer';
+import fs from "fs";
+import clear from "clear";
+import inquirer from "inquirer";
 
-import { Intro } from '../../utils/interactiveOutputs';
+import { Intro } from "../../utils/interactiveOutputs";
 
 const createFolder = (path) => {
-    fs.mkdirSync(process.cwd()+"/src"+path,{ recursive:true}, (error)=>{
-        if(error){
-            console.error("ERROR OCCURED:",error);
-            return false;
-        }
-        else {
-            return true;
-        }
-    })
-}
+  fs.mkdirSync(process.cwd() + "/src" + path, { recursive: true }, (error) => {
+    if (error) {
+      console.error("ERROR OCCURED:", error);
+      return false;
+    } else {
+      return true;
+    }
+  });
+};
 
 const componentTemplate = (component) => {
-    return (
-    `import React from 'react';\n\nconst ${component} = () => {\n\treturn (\n\t\t<div>${component}</div>\n\t)\n};\n\nexport default ${component};`
-    )
-}
+  return `import React from 'react';\n\nconst ${component} = () => {\n\treturn (\n\t\t<div>${component}</div>\n\t)\n};\n\nexport default ${component};`;
+};
 
 const routesTemplate = (pages) => {
-    return (
-    `export default {${pages.map(page=>{return (`\n\t${page.name}: '${page.route}'`)})}\n};`
-    )
-}
+  return `export default {${pages.map((page) => {
+    return `\n\t${page.name}: '${page.route}'`;
+  })}\n};`;
+};
 
-const createPageFile = ({component}) => {
-    fs.writeFile(process.cwd()+`/src/pages/${component}.js`,componentTemplate(component),(error)=>{
-        if(error){
-            console.log("ERROR OCCURED:",error);
-            return false;
-        }
-        else {
-            return true;
-        }
-    })
-}
+const createComponentFile = (component) => {
+  fs.writeFile(
+    process.cwd() + `/src/components/${component}.js`,
+    componentTemplate(component),
+    (error) => {
+      if (error) {
+        console.log("ERROR OCCURED:", error);
+        return false;
+      } else {
+        return true;
+      }
+    }
+  );
+};
+
+const createPageFile = ({ component }) => {
+  fs.writeFile(
+    process.cwd() + `/src/pages/${component}.js`,
+    componentTemplate(component),
+    (error) => {
+      if (error) {
+        console.log("ERROR OCCURED:", error);
+        return false;
+      } else {
+        return true;
+      }
+    }
+  );
+};
 
 const createRouteFile = (pages) => {
-    console.log("HEREEE")
-    fs.writeFile(process.cwd()+`/src/routes/index.js`,routesTemplate(pages),(error)=>{
-        if(error){
-            console.log("ERROR OCCURED:",error);
-            return false;
-        }
-        else {
-            return true;
-        }
-    })
-}
+  console.log("HEREEE");
+  fs.writeFile(
+    process.cwd() + `/src/routes/index.js`,
+    routesTemplate(pages),
+    (error) => {
+      if (error) {
+        console.log("ERROR OCCURED:", error);
+        return false;
+      } else {
+        return true;
+      }
+    }
+  );
+};
 
-const init = async (showIntro=true) => {
-    
-    if (showIntro) Intro();
-
+const init = async (showIntro = true) => {
+clear();
+  if (showIntro) Intro();
+  /*
     const pages = [
         {
             name: "home",
@@ -70,51 +88,116 @@ const init = async (showIntro=true) => {
         }
     ];
     
-    console.log("\n Let's start by creating your pages. Type *qa to end the process right there:\n")
-    let flag=1;/*
-    while(flag){
-        console.log("\n")
-        let answers = await inquirer.prompt([
-            {
-                name: "page",
-                message: "Name the page component:",
-            },
-            {
-                name:"route",
-                message:"Which route should it point to:"
-            }
-        ]);
-        try{
-            if (answers.page === "*qa" || answers.route === "*qa"){
-                flag=0;
-                continue;
-            }
-            else if (answers.page === "" || answers.route === "" ){
-                console.error("Either page or route in the wrong format!\n");
-            }
-            else {
-                const page = {
-                    name: answers.page.toLowerCase(),
-                    component:answers.page,
-                    route:answers.route
-                }
-            }
-        }
-        catch(err){
-            console.log("ERROR", err)
-        }
-    }*/
+    const components = ["Navbar", "ProjectCard"];
+*/
 
-    console.info("\n✅ Done");
-    console.log("\nPages:",pages);
+  const pages = [];
+  const components = [];
 
-    //flag= createPagesFolder(pages);
-    if(true) {
-        pages.forEach(page => createPageFile(page));
-        flag = createFolder('/routes');
-        console.log("flag")
-        if(true) createRouteFile(pages);
+  let flag = 1;
+  console.log("\n");
+
+  let answer, answers;
+
+  answer = await inquirer.prompt([
+    {
+      name: "check",
+      message: "Do you want to create pages? (y/n)",
+    },
+  ]);
+  let check = answer.check.toLowerCase();
+  if (check === "y" || check === "yes") {
+    flag = 1;
+  } else if (check === "n" || check === "no") {
+    flag = 0;
+  } else {
+    console.log("Couldn't understand you, answer with y/n only :( \n");
+    return;
+  }
+
+  while (flag) {
+    console.log("\n");
+    answers = await inquirer.prompt([
+      {
+        name: "page",
+        message: "Name the page component:",
+      },
+      {
+        name: "route",
+        message: "Which route should it point to:",
+      },
+    ]);
+    try {
+      if (answers.page === "*qa" || answers.route === "*qa") {
+        flag = 0;
+        continue;
+      } else if (answers.page === "" || answers.route === "") {
+        console.error("Either page or route is in the wrong format!\n");
+      } else {
+        const page = {
+          name: answers.page.toLowerCase(),
+          component: answers.page,
+          route: answers.route,
+        };
+        pages.push(page);
+      }
+    } catch (err) {
+      console.log("ERROR", err);
     }
-}
+  }
+  console.log("\n");
+  answer = await inquirer.prompt([
+    {
+      name: "check",
+      message: "Do you want to create components? (y/n)",
+    },
+  ]);
+
+  check = answer.check.toLowerCase();
+
+  if (check === "y" || check === "yes") {
+    flag = 1;
+  } else if (check === "n" || check === "no") {
+    flag = 0;
+  } else {
+    console.log("Couldn't understand you, answer with y/n only :( \n");
+    return;
+  }
+
+  while (flag) {
+    console.log("\n");
+    answers = await inquirer.prompt([
+      {
+        name: "component",
+        message: "Name the component:",
+      },
+    ]);
+    try {
+      if (answers.component === "*qa") {
+        flag = 0;
+        continue;
+      } else if (answers.component === "") {
+        console.error("Component is in the wrong format!\n");
+      } else {
+        components.push(answers.component);
+      }
+    } catch (err) {
+      console.log("ERROR", err);
+    }
+  }
+
+  console.info("\n Creating files..");
+
+  createFolder("/pages");
+  pages.forEach((page) => createPageFile(page));
+
+  createFolder("/routes");
+  createRouteFile(pages);
+
+  createFolder("/components");
+  components.forEach((component) => createComponentFile(component));
+
+  console.info("\n✅ Done");
+};
 
 export default init;
