b7c6f6a1dc7d95b024caa812eca0e0cf87a0a879
avinash-vk <avinash2000vk@gmail.com>
2021-05-11 10:46:28 +0530

    tweaked the help

--- src/commands/help/index.js
+++ src/commands/help/index.js
@@ -9,9 +9,27 @@ const menus = {
     ${chalk.blueBright('help')} ............... show help menu for a command
   `,
 
-  version:`\n${chalk.blueBright('version')} : show package version
+  init:`\n${chalk.redBright('init')} : create pages with routing + components for your already created react app
+  ${chalk.redBright('\nSyntax')}
+  ${chalk.greenBright('rsf init')}
+  ${chalk.redBright('\nDescription')}\n${chalk.whiteBright('The command adds a /components, /routes and /pages folder and updates creates the files accordingly. Rsf-cli looks for the /src directory and then creates the folders inside it. So ideally, you would cd into your react app directory and then run this command to initialize the components. ')}
+  `,
+
+  'create-react-app':`\n${chalk.redBright('create-react-app')} : creates a react app using the facebook/create-react-app template and 
+  ${chalk.redBright('\nSyntax')}
+  ${chalk.greenBright('rsf create-react-app <your-app-name>')}
+  ${chalk.redBright('\nDescription')}\n${chalk.whiteBright('Creates a react app using the cra template and runs rsf init in one command.')}
+  `,
+
+  help:`\n${chalk.redBright('help')} : Provides you help in times of need
+  ${chalk.redBright('\nSyntax')}
+  ${chalk.greenBright('rsf help [command]')}
+  ${chalk.redBright('\nDescription')}\n${chalk.whiteBright('You already reached here bro, you should know. ')}
+  `,
+
+  version:`\n${chalk.redBright('version')} : show package version
     ${chalk.redBright('\nSyntax')}
-    ${chalk.greenBright('rsf --version')}
+    ${chalk.greenBright('rsf version')}
   `,
 }
 
